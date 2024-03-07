234e3b9972dee66f97ca141bb29d8a8354a426b3
Maris <mau13@case.edu>
2024-03-07 11:11:03 -0500

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::{collections::HashMap, path::Path};
 
+use colored::Colorize;
+use hound::{Sample, WavSpec, WavWriter};
+use kiwisdr::{
+    kiwi::{AMTuning, AgcConfig, Station},
+    KiwiSdrBuilder, KiwiSdrError, KiwiSdrSndClient,
+};
 use serde::Deserialize;
 use url::Url;
-use kiwisdr::{kiwi::{AMTuning, AgcConfig, Station}, KiwiSdrBuilder, KiwiSdrError, KiwiSdrSndClient};
-use hound::{WavWriter, Sample, WavSpec};
-use colored::Colorize;
 
 #[derive(Debug, Deserialize, PartialEq, Eq)]
 enum WebSdrScraperType {
-    KiwiSDR
+    KiwiSDR,
 }
 
 #[derive(Debug, Deserialize)]
@@ -20,7 +23,6 @@ struct WebSdrConfig {
     password: Option<String>,
     frequency: i32,
     bandwidth: i32,
-
 }
 
 impl WebSdrConfig {
@@ -39,12 +41,12 @@ struct Config {
     data_dir: String,
     callsign: String,
     location: String,
-    stations: Vec<WebSdrConfig>
+    stations: Vec<WebSdrConfig>,
 }
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() {
-    simple_logger::init_with_level(log::Level::Info).unwrap();    
+    simple_logger::init_with_level(log::Level::Debug).unwrap();
 
     let config_contents = std::fs::read_to_string("config.toml").unwrap();
     let config = toml::from_str::<Config>(&config_contents).unwrap();
@@ -64,22 +66,29 @@ async fn main() {
             log::error!("Invalid URL: {:?}", station.endpoint);
             return;
         }
-    }   
+    }
 
     let mut map: HashMap<String, tokio::task::JoinHandle<()>> = HashMap::new();
 
     for station in config.stations {
-        log::info!("Connecting to {} at {}...", station.name.green(), station.endpoint);
+        log::info!(
+            "Connecting to {} at {}...",
+            station.name.green(),
+            station.endpoint
+        );
         let data_dir = config.data_dir.clone();
         let station_name = station.name.clone();
         let callsign = config.callsign.clone();
         let location = config.location.clone();
-        
+
         let task = tokio::spawn(async move {
             let client = station.create_client().await;
-            log::info!("Connected to {} at {}", station.name.green(), station.endpoint);
+            log::info!(
+                "Connected to {} at {}",
+                station.name.green(),
+                station.endpoint
+            );
             if let Ok(mut kiwi_sdr) = client {
-
                 tokio::select! {
                     _ = kiwi_sdr.wait_for_start() => {},
                     _ = tokio::time::sleep(std::time::Duration::from_secs(6)) => {
@@ -91,28 +100,43 @@ async fn main() {
                 let sample_rate = kiwi_sdr.get_sample_rate().await.unwrap();
                 log::info!("{}: Sample rate: {} Hz", station.name.green(), sample_rate);
 
-                log::info!("Tuning {} to {} kHz...", station.name.green(), station.frequency);
-                kiwi_sdr.tune(Station::AM(AMTuning {
-                    bandwidth: station.bandwidth,
-                    freq: station.frequency as f64
-                })).await.unwrap();
-                
+                log::info!(
+                    "Tuning {} to {} kHz...",
+                    station.name.green(),
+                    station.frequency
+                );
+                kiwi_sdr
+                    .tune(Station::AM(AMTuning {
+                        bandwidth: station.bandwidth,
+                        freq: station.frequency as f64,
+                    }))
+                    .await
+                    .unwrap();
+
                 kiwi_sdr.set_callsign(&callsign).await.unwrap();
                 kiwi_sdr.set_location(&location).await.unwrap();
 
-                kiwi_sdr.configure_agc(AgcConfig::new(false, false, -20, 6, 1000, 10)).await.unwrap();
+                kiwi_sdr
+                    .configure_agc(AgcConfig::new(false, false, -20, 6, 1000, 10))
+                    .await
+                    .unwrap();
                 kiwi_sdr.set_compression(false).await.unwrap();
 
                 // TODO on close
                 // TODO keep track of how many connected
                 // TODO reconnecting
 
-                let mut writer = WavWriter::create(Path::new(&data_dir).join(format!("{}_{}.wav", station.name, station.frequency)), WavSpec {
-                    channels: 1,
-                    sample_rate: sample_rate,
-                    bits_per_sample: 16,
-                    sample_format: hound::SampleFormat::Int,
-                }).unwrap();
+                let mut writer = WavWriter::create(
+                    Path::new(&data_dir)
+                        .join(format!("{}_{}.wav", station.name, station.frequency)),
+                    WavSpec {
+                        channels: 1,
+                        sample_rate: sample_rate,
+                        bits_per_sample: 16,
+                        sample_format: hound::SampleFormat::Int,
+                    },
+                )
+                .unwrap();
 
                 let kiwi_sdr_arc = std::sync::Arc::new(tokio::sync::Mutex::new(kiwi_sdr));
 
@@ -128,13 +152,13 @@ async fn main() {
                             return;
                         }
 
-                        log::info!("{}: Sending keepalive", station_name.green());
+                        log::debug!("{}: Sending keepalive", station_name.green());
                         kiwi.send_keepalive().await.unwrap();
                     }
                 });
 
                 let kiwi_sdr = kiwi_sdr_arc.clone();
-                
+
                 while let Ok(data) = kiwi_sdr.lock().await.get_sound_data().await {
                     let mut writer = writer.get_i16_writer(data.len() as u32);
                     for sample in data {
@@ -146,7 +170,6 @@ async fn main() {
         });
 
         map.insert(station_name.clone(), task);
-
     }
 
     loop {
@@ -169,9 +192,5 @@ async fn main() {
                 return;
             }
         }
-
-        
     }
-
-
 }
